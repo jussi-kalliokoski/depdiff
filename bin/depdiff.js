5db08174accc49d27465b2cbf235c64ceaffa376
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2014-04-01 12:43:57 +0300

    Added a CLI argument to allow writing to a file.
    Closes #3.

--- bin/depdiff.js
+++ bin/depdiff.js
@@ -6,6 +6,12 @@ var fs = require("fs");
 var _ = require("lodash");
 var depdiff = require("../lib");
 
+var outputStream = process.stdout;
+
+var write = function (data) {
+    outputStream.write(data);
+};
+
 var actions = module.exports = {
     displayHelp: function () {
         process.stderr.write(
@@ -14,12 +20,16 @@ var actions = module.exports = {
             "Available commands:\n" +
             "  help                             Displays this message.\n" +
             "  list-dependencies                Prints the list of package information into stdout as JSON.\n" +
-            "  list-changes <old-package-path>  Prints the list of package changes between current and old version (as provided by the JSON in the old-package-path).\n"
+            "  list-changes <old-package-path>  Prints the list of package changes between current and old version (as provided by the JSON in the old-package-path).\n" +
+            "\n" +
+            "Options:\n" +
+            "  -o, --output <file>              Prints the results into a file instead of stdout.\n" +
+            "\n"
         );
     },
 
     listDependencies: function () {
-        process.stdout.write(JSON.stringify(depdiff.listDependencies()));
+        write(JSON.stringify(depdiff.listDependencies()));
     },
 
     listChanges: function (filepath) {
@@ -27,8 +37,8 @@ var actions = module.exports = {
         var newDependencies = depdiff.listDependencies();
         var changes = depdiff.listChanges(oldDependencies, newDependencies);
         _.each(changes, function (change) {
-          var sign = change.type === "old" ? "-" : "+";
-          process.stdout.write(sign + " " + change.path + " (" + change.version + ")\n");
+            var sign = change.type === "old" ? "-" : "+";
+            write(sign + " " + change.path + " (" + change.version + ")\n");
         });
     },
 };
@@ -43,6 +53,13 @@ var availableCommands = {
 
 var isBeingRunAsAnExecutable = require.main === module;
 if ( isBeingRunAsAnExecutable ) {
+    var args = require("minimist")(process.argv.slice(3));
+    var outputFile = args.o || args.output;
+    if ( outputFile && typeof outputFile === "string" ) {
+        outputStream = fs.createWriteStream(outputFile, {
+            encoding: "utf8"
+        });
+    }
     var command = process.argv[2];
 
     if ( !_.has(availableCommands, command) ) {
